95faaaa89f7317fdbe5b3cad164b20b0e699cf39
ahmadalzoubi31 <ahmadalzoubi31@gmail.com>
2025-06-19 19:39:26 +0300

    refactor(env): update environment variable naming for consistency
    - Rename NEXT_PUBLIC_ENV_NODE to NEXT_PUBLIC_NODE_ENV in .env for clarity
    - Update getBackendUrl utility to reference the new environment variable name, ensuring correct backend URL retrieval based on environment

--- src/utils/getBackendUrl.ts
+++ src/utils/getBackendUrl.ts
@@ -1,5 +1,5 @@
 export const getBackendUrl = (path: string) => {
-  if (process.env.NEXT_PUBLIC_ENV_NODE === "prod") {
+  if (process.env.NEXT_PUBLIC_NODE_ENV === "prod") {
     return `${process.env.NEXT_PUBLIC_BACKEND_URL_PROD}${path}`;
   } else {
     return `${process.env.NEXT_PUBLIC_BACKEND_URL}${path}`;
